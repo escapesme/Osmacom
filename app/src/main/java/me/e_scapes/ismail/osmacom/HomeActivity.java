5b6bdd3f4732da1f7f6b43d1d8232f53374b31aa
Osmacom <info@e-scapes.me>
2018-05-28 15:26:19 +0200

    frist

--- app/src/main/java/me/e_scapes/ismail/osmacom/HomeActivity.java
+++ app/src/main/java/me/e_scapes/ismail/osmacom/HomeActivity.java
@@ -1,7 +1,10 @@
 package me.e_scapes.ismail.osmacom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gotomain(null);
+        return super.onTouchEvent(event);
+    }
+
+
+    public void gotomain(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+
     }
 }
